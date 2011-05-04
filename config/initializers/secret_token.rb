ed6219d458a57e21a413856f93af2fb788d96ba3
Tilo Sloboda <tilo.sloboda@gmail.com>
2011-05-04 11:46:18 -0700

    initial system with Mongoid, Devise and User model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoDeviseTest::Application.config.secret_token = '240b41c77773c77e7a78db758f0fa3d58def5b02b5f018f280dc2a3c625d1bd4d97aa90c0e69fd1b15b415f903d8a017d3bab18311b0891a652ee23054db0d52'
